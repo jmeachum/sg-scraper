121ec984b23dc4a3bd7250501cc4df53ba977a4f
jmeachum <joshua.meachum@gmail.com>
2023-01-22 17:17:05 +0000

    Refactor vimeo class

--- src/vimeo.py
+++ src/vimeo.py
@@ -20,19 +20,28 @@ class Vimeo(object):
         self.audio_url = None
         self._base_cdn_url = None
 
-    def get_urls_and_title(self):
+    @classmethod
+    def parse(cls, vimeo_url, session):
+        cls(vimeo_url, session)._parse()
+        return cls
+    
+    def _parse(self):
         self._get_vimeo_url()
         self._get_window_player_config()
         self._set_attributes()
         self._get_audio_url()
+
+    def get_urls_and_title(self):
+        if not self.video_url or not self.audio_url or not self.video_title:
+            self._parse()
         return self.video_url, self.audio_url, self.video_title
-    
+
     def _get_window_player_config(self):
         logging.info("Searching for script tags")
         soup = BeautifulSoup(self.content, "html.parser")
         script_tag = soup.find_all("script")
         if len(script_tag) > 0:
-            logging.info("Script tags parsed")
+            logging.info("Script tags found and parsed")
             logging.info("Search tags for cdn_url regex")
             for tag in script_tag:
                 result = re.search(r'({\s*"cdn_url".*?});', tag.string)
@@ -45,15 +54,19 @@ class Vimeo(object):
 
 
     def _set_attributes(self):
-        logging.info("Setting video quality dictionary from source")
+        logging.info("Setting video_quality_profiles attribute")
         streams = self._window_player_config['request']['files']['dash']['streams']
         self.video_quality_profiles = {item['quality'].lower(): item['id'] for item in streams}
-        logging.debug(f"Finished building quality map: {self.video_quality_profiles}")
-        logging.info("Getting highest quality video")
+        logging.info("Finished setting video_quality_profiles attribute")
+        logging.debug(f"{self.video_quality_profiles}")
+        
+        logging.info("Setting highest_video_quality attribute")
         self._get_highest_quality_id()
-        logging.info('Setting video title attribute')
+        logging.debug(f"highest_video_quality profile id is {self._highest_video_quality}")
+
+        logging.info('Setting video_title attribute')
         self.video_title = self._window_player_config['video']['title']
-        logging.debug(f'Set video title attribute to: {self.video_title}')
+        logging.debug(f'video_title attribute set to: {self.video_title}')
         default_cdn = self._window_player_config['request']['files']['dash']['default_cdn']
         self._cdn_stream_url = self._window_player_config['request']['files']['dash']['cdns'][default_cdn]['url']
         if base_url_search_group := re.search(
@@ -73,11 +86,10 @@ class Vimeo(object):
             best_sample_rate = [item for item in resp_json["audio"] if item['sample_rate'] in best_sample_rate]
             best_bitrate = [max(item["bitrate"] for item in best_sample_rate)][0]
             best_bitrate_id = [item for item in best_sample_rate if item['bitrate'] == best_bitrate]
-            # best_bitrate = [max(item["bitrate"] for item in resp_json["audio"])][0]
-            # best_bitrate_id = [item for item in resp_json["audio"] if item['bitrate'] == best_bitrate]
             self.audio_url = f'{self._base_cdn_url}parcel/audio/{best_bitrate_id[0].get("id")}.mp4'
-        except HTTPError as e:
-            logging.exception(msg=f"Failed to get url {self.url}", exc_info=e)
+            logging.info(f"Audio url: {self.audio_url}")
+        except HTTPError:
+            logging.exception(f"Failed to get url {self.url}", exc_info=True)
 
     def _get_vimeo_url(self):
         try:
@@ -87,12 +99,17 @@ class Vimeo(object):
             logging.info(f"Response from {self.url}, was successful")
             logging.debug(f'{resp.text}')
             self.content = resp.text
-        except HTTPError as e:
-            logging.exception(msg=f"Failed to get url {self.url}", exc_info=e)
+        except HTTPError:
+            logging.exception(f"Failed to get url {self.url}", exc_info=True)
 
     def _get_highest_quality_id(self):
+        logging.info("Searching for highest video quality")
         for resolution in self._video_resolutions:
             self._highest_video_quality = self.video_quality_profiles.get(resolution, None)
             if self._highest_video_quality:
-                break
+                logging.info("Set highest_video_quality")
+                logging.info(f"{self._highest_video_quality}")
+                return
+        logging.info("Failed to find highest quality video")
+        logging.debug(f"{self.video_quality_profiles}")
     
